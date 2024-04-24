da4b1edf7af82016938d7e22261da0e893f8ed67
Yechan Park <pych05@unist.ac.kr>
2024-04-25 06:30:13 +0900

    initial commit

---
+++ milestone2/src/main/java/com/group2/milestone2/Milestone2Application.java
@@ -0,0 +1,13 @@
+package com.group2.milestone2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Milestone2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Milestone2Application.class, args);
+	}
+
+}
---
+++ milestone2/src/test/java/com/group2/milestone2/Milestone2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group2.milestone2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Milestone2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
